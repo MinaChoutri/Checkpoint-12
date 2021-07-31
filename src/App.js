817b28e5e910af1caa7436f189ff2fcc1168772a
Choutri Amina <amina.choutri@gmail.com>
2021-07-31 15:29:32 +0200

    Checkpoint12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="btn-group"
+        role="group"
+        aria-label="Basic outlined example"
+      >
+        <button type="button" className="btn btn-outline-primary">
+          Left
+        </button>
+        <button type="button" className="btn btn-outline-primary">
+          Middle
+        </button>
+        <button type="button" className="btn btn-outline-primary">
+          Right
+        </button>
+      </div>
     </div>
   );
 }
